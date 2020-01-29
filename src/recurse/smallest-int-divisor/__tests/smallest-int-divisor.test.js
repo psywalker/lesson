b21db72feb9cfaea9b5a0171c83e8bbd6e99eb7d
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2020-01-29 14:02:12 +0300

    smallest-int-divisor: Wrote a solution and tests

--- src/recurse/smallest-int-divisor/__tests/smallest-int-divisor.test.js
+++ src/recurse/smallest-int-divisor/__tests/smallest-int-divisor.test.js
@@ -1,5 +1,13 @@
 import smallestIntDivisior from '../smallest-int-divisor';
 
 test('smallestIntDivisior empty argument', () => {
-  expect(smallestIntDivisior()).toBeFalsy();
+  expect(smallestIntDivisior()).toBeNull();
+  expect(smallestIntDivisior('5')).toBeNull();
+  expect(smallestIntDivisior(1)).toBe(1);
+  expect(smallestIntDivisior(5)).toBe(5);
+  expect(smallestIntDivisior(9)).toBe(3);
+  expect(smallestIntDivisior(22)).toBe(2);
+  expect(smallestIntDivisior(5)).toBeLessThanOrEqual(5);
+  expect(typeof smallestIntDivisior(5)).toBe('number');
+  expect(5 % smallestIntDivisior(5)).toBe(0);
 });
--- src/recurse/smallest-int-divisor/smallest-int-divisor.js
+++ src/recurse/smallest-int-divisor/smallest-int-divisor.js
@@ -1,5 +1,11 @@
-const smallestIntDivisior = (sum) => {
-  if (!sum) return false
+const smallestIntDivisior = (n) => {
+  if (!n || typeof n !== 'number') return null;
+  const iter = (num, int) => {
+    if (num === 1) return int;
+    if (!(n % num)) return iter(num - 1, num);
+    return iter(num - 1, int);
+  };
+  return iter(n, n);
 };
 
 export default smallestIntDivisior;
