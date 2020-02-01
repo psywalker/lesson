56f9086c90355d273bb5906572f01d444761b0dd
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2020-02-01 14:55:25 +0300

    two-integers: Changed the numbers on the lines in the tests

--- src/imperative/two-integers/__tests/two-integers.test.js
+++ src/imperative/two-integers/__tests/two-integers.test.js
@@ -1,6 +1,6 @@
 import twoIntegers from '../two-integers';
 
 test('twoIntegers', () => {
-  expect(twoIntegers(5, 1)).toBe('5, 4, 3, 2, 1');
-  expect(twoIntegers(1, 5)).toBe('1, 2, 3, 4, 5');
+  expect(twoIntegers('5', '1')).toBe('5, 4, 3, 2, 1');
+  expect(twoIntegers('1', '5')).toBe('1, 2, 3, 4, 5');
 });
