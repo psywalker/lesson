5ed58076dc63ac3945248603cbda1346797f4cd1
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2020-02-03 16:20:27 +0300

    two-integers: added two methods

--- src/recurse/two-integers/__tests/two-integers.test.js
+++ src/recurse/two-integers/__tests/two-integers.test.js
@@ -1,6 +1,11 @@
-import twoIntegers from '../two-integers';
+import { twoIntegers, twoIntegers2 } from '../two-integers';
 
 test('twoIntegers', () => {
   expect(twoIntegers('5', '1')).toBe('5, 4, 3, 2, 1');
   expect(twoIntegers('1', '5')).toBe('1, 2, 3, 4, 5');
+});
+
+test('twoIntegers2', () => {
+  expect(twoIntegers2('5', '1')).toBe('5, 4, 3, 2, 1');
+  expect(twoIntegers2('1', '5')).toBe('1, 2, 3, 4, 5');
 });
--- src/recurse/two-integers/two-integers.js
+++ src/recurse/two-integers/two-integers.js
@@ -1,8 +1,16 @@
-const twoIntegers = (n1, n2) => {
-  const num1 = +n1;
-  const num2 = +n2;
-  if (num1 === num2) return num1;
-  if (num1 > num2) return `${num1}, ${twoIntegers(num1 - 1, num2)}`;
-  return `${num1}, ${twoIntegers(num1 + 1, num2)}`;
+export const twoIntegers = (n1, n2) => {
+  const iter = (num1, num2) => {
+    if (num1 === num2) return num1;
+    return Number(num1) > Number(num2)
+      ? `${num1}, ${iter(Number(num1) - 1, Number(num2))}`
+      : `${num1}, ${iter(Number(num1) + 1, Number(num2))}`;
+  };
+  return iter(n1, n2);
 };
-export default twoIntegers;
+
+export const twoIntegers2 = (n1, n2) => {
+  if (Number(n1) === Number(n2)) return n1;
+  return Number(n1) > Number(n2)
+    ? `${n1}, ${twoIntegers2(Number(n1) - 1, Number(n2))}`
+    : `${n1}, ${twoIntegers2(Number(n1) + 1, Number(n2))}`;
+};
