49d5b7df761d611dd4388229d342c91d7edc22d7
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2020-02-04 14:37:12 +0300

    two-integers: added three methods

--- src/recurse/two-integers/__tests/two-integers.test.js
+++ src/recurse/two-integers/__tests/two-integers.test.js
@@ -1,11 +1,16 @@
-import { twoIntegers, twoIntegers2 } from '../two-integers';
+import { twoIntegers1, twoIntegers2, twoIntegers3 } from '../two-integers';
 
-test('twoIntegers', () => {
-  expect(twoIntegers('5', '1')).toBe('5, 4, 3, 2, 1');
-  expect(twoIntegers('1', '5')).toBe('1, 2, 3, 4, 5');
+test('twoIntegers1', () => {
+  expect(twoIntegers1('5', '1')).toBe('5, 4, 3, 2, 1');
+  expect(twoIntegers1('1', '5')).toBe('1, 2, 3, 4, 5');
 });
 
 test('twoIntegers2', () => {
   expect(twoIntegers2('5', '1')).toBe('5, 4, 3, 2, 1');
   expect(twoIntegers2('1', '5')).toBe('1, 2, 3, 4, 5');
+});
+
+test('twoIntegers3', () => {
+  expect(twoIntegers3('5', '1')).toBe('5, 4, 3, 2, 1');
+  expect(twoIntegers3('1', '5')).toBe('1, 2, 3, 4, 5');
 });
--- src/recurse/two-integers/two-integers.js
+++ src/recurse/two-integers/two-integers.js
@@ -1,16 +1,27 @@
-export const twoIntegers = (n1, n2) => {
-  const iter = (num1, num2) => {
-    if (num1 === num2) return num1;
-    return Number(num1) > Number(num2)
-      ? `${num1}, ${iter(Number(num1) - 1, Number(num2))}`
-      : `${num1}, ${iter(Number(num1) + 1, Number(num2))}`;
+export const twoIntegers1 = (a, b) => {
+  const iter = (i1, i2) => {
+    if (i1 === i2) return i1;
+    return i1 > i2
+      ? `${i1}, ${iter(i1 - 1, i2)}`
+      : `${i1}, ${iter(i1 + 1, i2)}`;
   };
-  return iter(n1, n2);
+  return iter(Number(a), Number(b));
 };
 
-export const twoIntegers2 = (n1, n2) => {
+const makeRange = (i1, i2) => {
+  if (i1 === i2) return i1;
+  return i1 > i2
+    ? `${i1}, ${makeRange(i1 - 1, i2)}`
+    : `${i1}, ${makeRange(i1 + 1, i2)}`;
+};
+const parseStringsToIntegers = (...strArgs) => strArgs.map(Number);
+export const twoIntegers2 = (a, b, formResult = makeRange, parser = parseStringsToIntegers) => (
+  formResult(...parser(a, b))
+);
+
+export const twoIntegers3 = (n1, n2) => {
   if (Number(n1) === Number(n2)) return n1;
   return Number(n1) > Number(n2)
-    ? `${n1}, ${twoIntegers2(Number(n1) - 1, Number(n2))}`
-    : `${n1}, ${twoIntegers2(Number(n1) + 1, Number(n2))}`;
+    ? `${n1}, ${twoIntegers3(Number(n1) - 1, Number(n2))}`
+    : `${n1}, ${twoIntegers3(Number(n1) + 1, Number(n2))}`;
 };
