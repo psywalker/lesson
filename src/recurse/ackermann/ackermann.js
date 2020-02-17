33b4079540940107160bb6b835bd1dddc85cfe38
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2020-02-17 18:02:53 +0300

    ackermann: first itearation

--- src/recurse/ackermann/ackermann.js
+++ src/recurse/ackermann/ackermann.js
@@ -1,4 +1,5 @@
 const ackermann = (m, n) => {
+  if (m < 0 || n < 0) return false;
   if (m === 0) return n + 1;
   if (m > 0 && n === 0) return ackermann(m - 1, 1);
   if (m > 0 && n > 0) return ackermann(m - 1, ackermann(m, n - 1));
